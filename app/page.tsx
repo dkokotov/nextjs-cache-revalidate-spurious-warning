1598176996966814f13fe11eaaac6511b14f3b27
Daniel Kokotov <dkokotov@gmail.com>
2023-11-06 14:29:05 -0800

    added repro example

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default async function Home() {
+  const req = new Request('https://example.com/page');
+
+  await fetch(req, {
+    next: {
+      revalidate: 1000
+    }
+  });
+
+  await fetch('https://example.com/page2', {
+    next: {
+      revalidate: 1000
+    }
+  });
+
+  return <div>Hello</div>
 }
